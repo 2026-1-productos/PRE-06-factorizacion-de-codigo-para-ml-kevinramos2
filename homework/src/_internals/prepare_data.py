a24af8ec9eb4473352d97ba65b2196ea5581bb86
Kevin Ramos <141960759+kevinramos2@users.noreply.github.com>
2026-05-30 09:30:59 -0500

    up

--- homework/src/_internals/prepare_data.py
+++ homework/src/_internals/prepare_data.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
+TEST_SIZE= 0.25
+RANDOM_STATE= 123456
+
 def prepare_data():
     url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
     df = pd.read_csv(url, sep=";")
@@ -16,8 +19,8 @@ def prepare_data():
     x_train, x_test, y_train, y_test = train_test_split(
         x,
         y,
-        test_size=0.25,
-        random_state=123456,
+        test_size=TEST_SIZE,
+        random_state=RANDOM_STATE,
     )
 
     return x_train, x_test, y_train, y_test
